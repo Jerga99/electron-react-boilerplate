54601eb8093a18e4fc9e570871e792bdb25dcd74
JollyRen <jollyren@gmail.com>
2022-03-14 09:22:11 +0100

    first crypt commit

---
+++ src/js/Crypt.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default () => {
+  return (
+    <div>
+      <p> Nothing hidden yet!</p>
+    </div>
+  )
+}
--- src/js/Login.js
+++ src/js/Login.js
@@ -24,7 +24,7 @@ export default function Login() {
 
   return isLoggedIn ? (
     <div>
-      <p>logged in</p>
+      <Crypt />
     </div>
   ) : (
     <div>
